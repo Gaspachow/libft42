4b7659d074d4ce5de327c703fbcc8a549f2dcb37
gaspachow <gaspardsmets@gmail.com>
2019-09-02 16:38:18 +0200

    ft_strncmp addition + gitignore update

--- libft.h
+++ libft.h
@@ -7,6 +7,7 @@ size_t	ft_strlen(char *str);
 void	ft_putstr(char *str);
 char	*ft_strcpy(char *dest, const char *src);
 int     ft_strcmp(const char *str1, const char *str2);
+int     ft_strncmp(const char *str1, const char *str2, size_t n);
 
 
 #endif
