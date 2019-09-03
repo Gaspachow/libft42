2a3770d7bc061ea33e944f9c4aa0fd99b1437eda
gaspachow <gaspardsmets@gmail.com>
2019-09-03 14:49:20 +0200

    typo fix on lobft.h

--- libft.h
+++ libft.h
@@ -1,7 +1,7 @@
 #ifndef LIBFT_H
-#define LIBFT_H
+# define LIBFT_H
 
-#include <string.h>
+# include <string.h>
 
 size_t	ft_strlen(char *str);
 void	ft_putstr(char *str);
@@ -10,9 +10,5 @@ int     ft_strcmp(const char *str1, const char *str2);
 int     ft_strncmp(const char *str1, const char *str2, size_t n);
 int     ft_toupper(char c);
 int     ft_tolower(char c);
-char	*ft_strncpy(char *dest, const char *src, size_t n)
-
-
-
-
+char	*ft_strncpy(char *dest, const char *src, size_t n);
 #endif
