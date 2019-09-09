883328acddbb7c6e0bdfdec666de8ed4ad5dc9fc
gaspachow <gaspardsmets@gmail.com>
2019-09-09 21:11:13 +0200

    ft_isascii addition

--- libft.h
+++ libft.h
@@ -18,6 +18,7 @@ size_t  ft_strlcat(char *dest, const char *src, size_t size);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 char    *ft_strchr(const char *str, int c);
 char    *ft_strrchr(const char *str, int c);
+int     ft_isascii(int c);
 
 
 #endif
