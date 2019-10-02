5a0fc10f6092ed5197fcd3a0d557dbee2fc5aabb
gaspachow <gaspardsmets@gmail.com>
2019-10-02 19:42:35 +0200

    Fix unsigned char support to strcmp and strncmp

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -4,9 +4,9 @@ int ft_strcmp(const char *str1, const char *str2)
     while (*str1 && *str2)
     {
         if (*str1 != *str2)
-            return (*str1 - *str2);
+            return ((unsigned char)*str1 - (unsigned char)*str2);
         str1++;
         str2++;
     }
-    return (*str1 - *str2);
+    return ((unsigned char)*str1 - (unsigned char)*str2);
 }
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -7,9 +7,9 @@ int ft_strncmp(const char *str1, const char *str2, size_t n)
     while (*str1 && *str2 && --n)
     {
         if (*str1 != *str2)
-            return (*str1 - *str2);
+            return ((unsigned char)*str1 - (unsigned char)*str2);
         str1++;
         str2++;
     }
-    return (*str1 - *str2);
+    return ((unsigned char)*str1 - (unsigned char)*str2);
 }
