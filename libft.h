3a7eb2bf27b0fb1cb456db18e0ffcc70d4b0bcef
Gaspard SMETS <gsmets@student.42.fr>
2019-10-10 19:18:31 +0200

    ft_lstiter to be checked

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gsmets <gsmets@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 11:25:57 by gsmets            #+#    #+#             */
-/*   Updated: 2019/10/10 18:39:39 by gsmets           ###   ########.fr       */
+/*   Updated: 2019/10/10 19:18:17 by gsmets           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,4 +71,5 @@ t_list			*ft_lstlast(t_list *lst);
 void			ft_lstadd_back(t_list **alst, t_list *new);
 void			ft_lstdelone(t_list *lst, void (*del)(void *));
 void			ft_lstclear(t_list **lst, void (*del)(void *));
+void			ft_lstiter(t_list *lst, void (*f)(void *));
 #endif
