f65a9032b5267044bb6cb5d55c17d2f4f21d224a
Gaspard SMETS <gsmets@e2r6p9.42.fr>
2019-10-10 14:06:35 +0200

    ft_lstnew addition

---
+++ ft_lstnew_bonus.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsmets <gsmets@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/10 12:21:08 by gsmets            #+#    #+#             */
+/*   Updated: 2019/10/10 13:40:03 by gsmets           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	if (!(new = malloc(sizeof(t_list))))
+		return (NULL);
+	new->content = content;
+	new->next = NULL;
+	return (new);
+}
