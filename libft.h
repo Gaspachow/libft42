11cfb7206b174d3b4bc70832abf6aa936dd836e7
Gaspard SMETS <gsmets@student.42.fr>
2019-10-10 16:32:22 +0200

    ft_lstadd_back done

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gsmets <gsmets@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 11:25:57 by gsmets            #+#    #+#             */
-/*   Updated: 2019/10/10 16:23:31 by gsmets           ###   ########.fr       */
+/*   Updated: 2019/10/10 16:27:04 by gsmets           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,5 +68,5 @@ t_list			*ft_lstnew(void *content);
 void			ft_lstadd_front(t_list **alst, t_list *new);
 int				ft_lstsize(t_list *lst);
 t_list			*ft_lstlast(t_list *lst);
-
+void			ft_lstadd_back(t_list **alst, t_list *new);
 #endif
