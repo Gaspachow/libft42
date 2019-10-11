60795d9bd8aee6d8e892ee3e1d917d372770c477
Gaspard SMETS <gsmets@student.42.fr>
2019-10-11 11:42:34 +0200

    ft_lstmap tentative

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: gsmets <gsmets@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/10 19:18:48 by gsmets            #+#    #+#             */
-/*   Updated: 2019/10/10 19:32:20 by gsmets           ###   ########.fr       */
+/*   Updated: 2019/10/11 11:42:16 by gsmets           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,24 @@
 
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(void *))
 {
-	t_list	*new;
+	t_list	*alst;
+	t_list	*newlst;
+	t_list	*tmp;
+
+	if (!lst || !f)
+		return (NULL);
+	tmp = (*f)(lst);
+	if(!(newlst = ft_lstnew(tmp->content)))
+		return (NULL);
+	alst = newlst;
+	lst = lst->next;
 	while (lst)
 	{
-
+		tmp = (*f)(lst);
+		if (!(newlst->next = ft_lstnew(tmp->content)))
+			return (NULL);
+		newlst = newlst->next;
+		lst = lst->next;
 	}
-	return (new) 
+	return (alst);
 }
